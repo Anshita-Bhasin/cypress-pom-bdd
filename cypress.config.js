983d5a0f1b473f12820fa261d5c158127db84bd1
Anshita-Bhasin <anshita.bhasin@gmail.com>
2025-07-27 01:14:51 +0400

    update jenkinsfile

--- cypress.config.js
+++ cypress.config.js
@@ -8,9 +8,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com/v1/",
     specPattern: 'cypress/e2e/features/**/*.feature',
-    specPattern: `cypress/e2e/**/*${tag}*.cy.js`,
-
-
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
 
